3f6c510ccce95c7b3e222c5f87a6213d889335bd
Morgan <morgan.batterham@gmail.com>
2020-05-05 17:37:01 +1000

    bug fixes in simpleSubCipher.py

--- simpleSubCipher.py
+++ simpleSubCipher.py
@@ -1,5 +1,5 @@
 # Simple Substitution Cipher
-# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+# https://www.nostarch.com/crackingcodes (BSD Licensed)
 
 import pyperclip, sys, random
 
@@ -9,28 +9,28 @@ LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 def main():
     myMessage = 'If a man is offered a fact which goes against his instincts, he will scrutinize it closely, and unless the evidence is overwhelming, he will refuse to believe it. If, on the other hand, he is offered something which affords a reason for acting in accordance to his instincts, he will accept it even on the slightest evidence. The origin of myths is explained in this way. -Bertrand Russell'
     myKey = 'LFWOAYUISVKMNXPBDCRJTQEGHZ'
-    myMode = 'encrypt' # Set to 'encrypt' or 'decrypt'
-    
-    is keyIsValid(myKey):
-        sys.exit('There is an error in the key or symbol set')
-    if myMode == 'encrypt'
+    myMode = 'encrypt' # Set to either 'encrypt' or 'decrypt'.
+
+    if not keyIsValid(myKey):
+        sys.exit('There is an error in the key or symbol set.')
+    if myMode == 'encrypt':
         translated = encryptMessage(myKey, myMessage)
-    elif myMode == 'decrypt'
+    elif myMode == 'decrypt':
         translated = decryptMessage(myKey, myMessage)
-        print('Using key %s' (myKey))
-        print('The %sed message is:' % (myMode))
-        print(translated)
-        pyperclip.copy(translated)
-        print()
-        print('This message has been copied to the clipboard')
-        
-        
+    print('Using key %s' % (myKey))
+    print('The %sed message is:' % (myMode))
+    print(translated)
+    pyperclip.copy(translated)
+    print()
+    print('This message has been copied to the clipboard')
+
+
 def keyIsValid(key):
     keyList = list(key)
     lettersList = list(LETTERS)
     keyList.sort()
     lettersList.sort()
-    
+
     return keyList == lettersList
 
 
@@ -50,14 +50,14 @@ def translateMessage(key, message, mode):
         # For decrypting, we can use the same code as encrypting. We
         # just need to swap where the key and LETTERS string are used.
         charsA, charsB = charsB, charsA
-        
-    # Loop through each symbol in the message:
+
+    # Loop through each symbol in message:
     for symbol in message:
         if symbol.upper() in charsA:
             # Encrypt/decrypt the symbol:
             symIndex = charsA.find(symbol.upper())
             if symbol.isupper():
-                translated += charsB[symIndex]
+                translated += charsB[symIndex].upper()
             else:
                 translated += charsB[symIndex].lower()
         else:
