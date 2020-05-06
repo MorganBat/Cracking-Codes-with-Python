e6bc7e73b52764af28768d11a593a5cdcc7225ac
Morgan <morgan.batterham@gmail.com>
2020-05-06 21:51:22 +1000

    developed simple subsitition hacker

--- simpleSubHacker.py
+++ simpleSubHacker.py
@@ -49,4 +49,108 @@ def addLettersToMapping(letterMapping, cipherword, candidate):
 
     for i in range(len(cipherword)):
         if candidate[i] not in letterMapping[cipherword[i]]:
-            letterMapping[cipherword[i]].append(candidate[i])    
+            letterMapping[cipherword[i]].append(candidate[i])
+            
+            
+            
+def intersectMappings(mapA, mapB):
+    # To intersect two maps, create a blank map and then add only the
+    # potential decryption letters if they exist in BOTH maps:
+    intersectedMapping = getBlankCipherletterMapping()
+    for letter in LETTERS:
+        
+        # An empty list means "any letter is possible". In this case just
+        # copy the other map entirely:
+        if mapA[letter] == []:
+            intersectedMapping[letter] = copy.deepcopy(mapB[letter])
+        elif mapB[letter] == []:
+            intersectedMapping[letter] = copy.deepcopy(mapA[letter])
+        else:
+            # If a letter in mapA[letter] exists in mapB[letter],
+            # add that letter to intersectedMapping[letter]:
+            for mappedLetter in mapA[letter]:
+                if mappedLetter in mapB[letter]:
+                    intersectedMapping[letter].append(mappedLetter)  
+                    
+    return intersectedMapping
+
+
+def removeSolvedLettersFromMapping(letterMapping):
+    # Cipherletters in the mapping that map to only one letter are
+    # "solved" and can be removed from the other letters.
+    # For example, if "A" maps to potential letters ['M, 'N'] and 'B'
+    # maps to ['N'], the we know that 'B' must map to 'N', so we can
+    # removed 'N' from the list of what 'A' could map to. So 'A' then maps
+    # to ['M']. Note that now that 'A' maps to only one letter, we can 
+    # remove 'M' from the list of letters for every other letter.
+    # This is why there is a loop that keeps reducing the map.
+    
+    loopAgain = True
+    while loopAgain:
+        # First assume that we will not loop again:
+        loopAgain = False
+        
+        # solvedLetters will be a list of uppercase letters that have one
+        # and only one possible mapping in letterMapping:
+        solvedLetters = []
+        for cipherletter in LETTERS:
+            if len(letterMapping[cipherletter]) == 1:
+                solvedLetters.sppend(letterMapping[cipherletter][0])
+                
+        # If a letter is solved, then it cannot possible be a potential
+        # decryption letter for a different ciphertext letter, so we
+        # should remove it from those other lists:
+        for cipherletter in LETTERS:
+            for s in solvedLetters:
+                if len(letterMapping[cipherletter]) != 1 and s in letterMapping[cipherletter]:
+                    letterMapping[cipherletter].remove(s)
+                if len(letterMapping) == 1:
+                    # A new letter is now solved, so loop again:
+                    loopAgain = True
+    return letterMapping
+
+
+def hackSimpleSub(message):
+    intersectedMap = getBlankCipherletterMapping()
+    cipherwordList = nonLettersOrSpacePattern.sub('', message.upper()).split()
+    for cipherword in cipherwordList:
+        # Get a new cipherletter mapping for each ciphertext word:
+        candidateMap = getBlankCipherletterMapping()
+        
+        wordPattern = makeWordPatterns.getWordPattern(cipherword) 
+        if wordPattern not in wordPatterns.allPatterns:
+            continue # This word was not in our dictionary, so continue
+        
+        # Add the letters of each candidate to the mapping:
+        for candidate in wordPatterns.allPatterns[wordPattern]:
+            addLettersToMapping(candidateMap, cipherword, candidate)
+            
+        # Intersecr the new mapping with the existing intersected mapping:
+        intersectedMap = intersectMappings(intersectedMap, candidateMap)
+        
+    # Remove any solved letters from the other lists:
+    return removeSolvedLettersFromMapping(intersectedMap)
+
+
+def decryptWithCipherLetterMapping(ciphertext, letterMapping):
+    # Return a string of the ciphertext decrypted with the letter mapping,
+    # with any ambiguous decrypted letters replaced with an underscore.
+    
+    # First create a simple sub key from the letterMapping mapping:
+    key = ['x'] * len(LETTERS)
+    for cipherletter in LETTERS:
+        if len(letterMapping[cipherletter]) == 1:
+            # If there's only one letter, add it to the key:
+            keyIndex = LETTERS.find(letterMapping[cipherletter][0])
+            key[keyIndex] = cipherletter
+        else:
+            ciphertext = ciphertext.replace(ciphertext.lower(), '_')
+            ciphertext = ciphertext.replace(ciphertext.upper(), '_')
+    key = ''.join(key)
+    
+    # With the key we've created, decrypt the ciphertext:
+    return simpleSubCipher.decryptMessage(key, ciphertext)
+
+
+if __name__ == '__main__':
+    main()
